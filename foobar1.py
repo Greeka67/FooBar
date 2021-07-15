8845bb48c70632643bca8b57cebad3123224194a
Greeka67 <igoshino@gmail.com>
2021-07-15 19:42:52 +0300

    Add files via upload

---
+++ foobar1.py
@@ -0,0 +1,17 @@
+def one(a,b):
+    if a%3!=0:
+        print(a,"foo")
+    else:
+        print(a,"bar")
+    a+=1
+    if a<=b:
+        one(a,b)
+
+
+x=input()
+if x.isdigit() and int(x)>0:
+    print("ввод верный")
+    x=int(x)
+    one(1,x)
+else:
+    print("неверный ввод")
